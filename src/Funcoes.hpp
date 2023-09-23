43b4de6bc58a0e330d3e5f0544994afb078260dd
joaomarcoshess <75141271+joaomarcoshess@users.noreply.github.com>
2023-09-22 21:19:20 -0300

    Update Funcoes.hpp

--- src/Funcoes.hpp
+++ src/Funcoes.hpp
@@ -1,1 +1,23 @@
-a ser colocado
+#include <bits/stdc++.h>
+
+#ifndef FUNCOES_HPP
+#define FUNCOES_HPP
+
+using namespace std;
+
+class Funcoes
+{
+  public:
+    Funcoes();
+
+    void Welcome();
+    wchar_t* getText(const locale& loc, string name);
+    unordered_set<wstring> getStopWords(const locale& loc);
+    unordered_set<wstring> getInput(const locale& loc);
+    void swapMinHeap(vector<pair<wstring, int>>& minHeap, const int& indexToRemove, const vector<pair<wstring, int>>& proxTopK, const int& indexMinHeap);
+    void makeFrequenciesMaps(const locale& loc, const int& numFiles, vector<unordered_map<wstring, int>>& frequenceMap);
+    void printHeap(vector<pair<wstring, int>> heap);
+    void makeHeaps(const int& k, const int& numFiles, const vector<unordered_map <wstring, int>>& frequenceMap, vector<vector<pair<wstring, int>>>& minHeap, vector<pair<wstring, int>>& proxTopK);
+};
+
+#endif
